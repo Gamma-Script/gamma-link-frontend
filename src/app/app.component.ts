dce2c69b22cceaac3c6965543ea71316bb9559ce
RicardoEstupinian <ricaeslo@gmail.com>
2020-11-10 01:12:26 +0100

    Proyecto base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var jQuery:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gamma-link-frontend';
+
+
+  ngOnInit(){
+    // SCRIPT PARA SIDE BAR TOGGLE
+    // NO ELIMINAR
+    (function($) {
+      "use strict";
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e) {
+          e.preventDefault();
+          $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
 }
