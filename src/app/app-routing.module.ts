f20615241d1df49fa5ec2654fb21cf548b310d76
Cesar Augusto Perez <pl19010@ues.edu.sv>
2020-11-17 11:45:18 -0600

    catalogo producto v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductoContainerComponent } from '../app/components/producto/producto-container/producto-container.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'productos', component : ProductoContainerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/producto/producto-container/producto-container.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
+
+@Component({
+  selector: 'app-producto-container',
+  templateUrl: './producto-container.component.html',
+  styleUrls: ['./producto-container.component.css']
+})
+export class ProductoContainerComponent implements OnInit {
+
+  productos : Producto[];
+
+  constructor(
+    private productoService : ProductoService
+  ) { }
+
+    
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  getProductos(){
+    this.productos = this.productoService.getProductos();
+  }
+
+
+
+
+
+
+
+
+  
+/*
+  deleteContainer(value:any){
+    console.log("Container data: "+value);
+    this.productService.deleteProduct(value);
+  }
+
+  addContainer(data:Product){
+    this.productService.addProduct(data);
+    console.log(data);
+  }
+*/
+}
