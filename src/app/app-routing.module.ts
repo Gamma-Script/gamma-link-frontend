87248d703cb02487edcdb7cf20180b43eca6b4b9
Pedro Alexander Zavaleta Perez <zp19013@ues.edu.sv>
2020-11-17 19:33:05 -0600

    Creacion del caso de uso Crear cuenta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CuentaContainerComponent } from './components/cuenta/cuenta-container/cuenta-container.component';
+import { CuentaFormComponent } from './components/cuenta/cuenta-form/cuenta-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'content', component : CuentaContainerComponent
+,
+children: [{
+  path: 'new', component: CuentaFormComponent
+}]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuentaContainerComponent } from './components/cuenta/cuenta-container/cuenta-container.component';
+import { CuentaFormComponent } from './components/cuenta/cuenta-form/cuenta-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuentaContainerComponent,
+    CuentaFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
